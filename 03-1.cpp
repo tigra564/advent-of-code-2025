6fc1ad73ad12200d99048a61ee88b3e20bc9584f
Sergey Myasnikov <tigra564@gmail.com>
2026-09-11 21:29:17 +0300

    Minor style fixes

--- 03-1.cpp
+++ 03-1.cpp
@@ -7,14 +7,12 @@ int main(int argc, char* argv[])
 	std::ifstream file = aoc::open_data(argc, argv);
 	std::string record;
 
-	auto result = 0ULL;
+	auto result = i64{0};
 	while (file >> record) {
 		auto first = std::max_element(record.begin(), record.end() - 1);
 		auto second = std::max_element(first + 1, record.end());
 
 		result += (*first - '0') * 10 + (*second - '0');
-
-		//std::cout << record << " " << *first << " " << *second << '\n';
 	}
 
 	std::cout << "Result: " << result << '\n';
--- 03-2.cpp
+++ 03-2.cpp
@@ -7,12 +7,14 @@ int main(int argc, char* argv[])
 	std::ifstream file = aoc::open_data(argc, argv);
 	std::string record;
 
-	auto result = 0ULL;
+	constexpr int ndigits = 12;
+
+	auto result = i64{0};
 	while (file >> record) {
-		auto joltage = 0ULL;
+		auto joltage = i64{0};
 
 		auto start = record.begin();
-		for (auto i = 11; i >= 0; i--) {
+		for (auto i = ndigits - 1; i >= 0; i--) {
 			auto digit_it = std::max_element(start, record.end() - i);
 			joltage = joltage * 10 + (*digit_it - '0');
 			start = digit_it + 1;

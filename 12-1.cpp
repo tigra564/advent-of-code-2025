8a70e2a51887909961eea729c668d72dd34adde8
Sergey Myasnikov <tigra564@gmail.com>
2026-09-13 21:53:31 +0300

    Minor style fixes

--- 12-1.cpp
+++ 12-1.cpp
@@ -8,7 +8,8 @@ int main(int argc, char* argv[])
 	std::string record;
 
 	// Just ignore first 30 lines which hold gift shapes
-	for (int i = 0; i < 30; i++) {
+	constexpr auto lines_to_skip = 30;
+	for (int i = 0; i < lines_to_skip; i++) {
 		std::getline(file, record);
 	}
 
@@ -16,15 +17,15 @@ int main(int argc, char* argv[])
 	while (std::getline(file, record)) {
 		auto x = record.find('x');
 		auto c = record.find(':');
-		auto w = stoi(record.substr(0, x));
-		auto h = stoi(record.substr(x + 1, c - x - 1));
+		auto w = std::stoi(record.substr(0, x));
+		auto h = std::stoi(record.substr(x + 1, c - x - 1));
 		auto max_blocks = (w / 3) * (h / 3);
 
 		std::stringstream quantities(record.substr(c+1));
 		auto num_blocks = 0;
 		std::string quant;
 		while (quantities >> quant)
-			num_blocks += stoi(quant);
+			num_blocks += std::stoi(quant);
 
 		if (num_blocks <= max_blocks)
 			result++;
